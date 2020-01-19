3ad2bd0a11d3ae43c34df47595aadf42149feacf
Awa Dieudonne M <dieudonneawa7@gmail.com>
2020-01-19 16:37:54 +0100

    setup signin reducer

--- src/components/auth/signUp/SignUp.tsx
+++ src/components/auth/signUp/SignUp.tsx
@@ -1,14 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import * as React from 'react';
-import { StyledSignUp } from '../styles';
+import Cookie from 'js-cookie';
 import { BrowserRouter, Link } from 'react-router-dom';
 import { Field, reduxForm, InjectedFormProps } from 'redux-form';
+import { StyledSignUp } from '../styles';
 import { connect } from 'react-redux';
 import { signUp } from '../../../actions/auth';
 import { User } from '../../../actions/interfaces';
 
 interface StateProps {
   user: User;
+  authError: string;
 }
 interface DispatchProps {
   signUp: (user: User) => Function;
@@ -32,16 +34,24 @@ export class SignUp extends React.Component<InjectedFormProps<User, Props> & Pro
   renderInput = ({ input, placeholder, type, meta }: any) => {
     return (
       <div className="form-input">
-        <input {...input} placeholder={placeholder} type={type}/>
+        <input {...input} placeholder={placeholder} type={type} />
         {this.renderError(meta)}
       </div>
     );
   }
-  
+
   onSubmit = async (formValues: User): Promise<void> => {
-    await this.props.signUp(formValues);
+    await this.props.signUp(formValues)
+      // @ts-ignore
+      .then((res) => {
+        if (res) {
+          const { token } = res.data.data.user;
+          Cookie.set('token', token, { expires: 7 });
+        }
+      });
+
   }
-  
+
   render() {
     return (
       <StyledSignUp>
@@ -53,11 +63,12 @@ export class SignUp extends React.Component<InjectedFormProps<User, Props> & Pro
         <div className="form-wrapper">
           <h3>Register</h3>
           <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
-              <Field type="text" name="name" component={this.renderInput} placeholder="Enter Full Name"/>
-              <Field type="email" name="email" component={this.renderInput} placeholder="Enter Email Address"/>
-              <Field type="password" name="password" component={this.renderInput} placeholder="Enter Password"/>
-              <Field type="password" name="confPassword" placeholder="Confirm Password" component={this.renderInput}/>
-            <input type="submit" value="Sign Up"/>
+            <Field type="text" name="name" component={this.renderInput} placeholder="Enter Full Name" />
+            <Field type="email" name="email" component={this.renderInput} placeholder="Enter Email Address" />
+            <Field type="password" name="password" component={this.renderInput} placeholder="Enter Password" />
+            <Field type="password" name="confPassword" placeholder="Confirm Password" component={this.renderInput} />
+            {this.props.authError && <div className="errors">{this.props.authError}</div>}
+            <input type="submit" value="Sign Up" />
           </form>
           <p>Have an account already? <Link to="/auth/sign_in">Sign In</Link></p>
         </div>
@@ -86,8 +97,8 @@ const validate = (formValues: User): User => {
   return errors;
 }
 
-const mapStateToProps = ({ auth: { user }}: any): StateProps => ({
-  user,
+const mapStateToProps = ({ auth: { user, authError } }: any): StateProps => ({
+  user, authError,
 });
 
 const connectedSignUp = reduxForm<{}, User, string>({
--- src/reducers/auth.ts
+++ src/reducers/auth.ts
@@ -4,16 +4,28 @@ import { Types } from "../actions/types";
 
 interface State {
   user: User;
+  authError: string;
 }
 
 const initialState = {
   user: null,
+  authError: '',
 }
 // @ts-ignore
 export const authReducer = (state: State = initialState, action: Action): State => {
   switch(action.type) {
-    case asyncActionNames(Types.SIGN_UP).success:
-      return { ...state, user: action.payload };
+    case asyncActionNames(Types.SIGN_UP).success: {
+      const { user } = action.payload.data;
+      return { ...state, user };
+    }
+    case asyncActionNames(Types.SIGN_UP).failure: {
+      const { error } = action.payload.error;
+      return { ...state, authError: error };
+    }
+    case asyncActionNames(Types.SIGN_IN).success: {
+      const { user } = action.payload.data;
+      return { ...state, user };
+    }
     default:
       return state;
   }
